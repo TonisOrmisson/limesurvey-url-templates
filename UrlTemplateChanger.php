5557a6e2a930d03cc1c6695730cc5a3765c49840
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2020-06-29 17:52:23 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- UrlTemplateChanger.php
+++ UrlTemplateChanger.php
@@ -27,25 +27,25 @@ class UrlTemplateChanger extends PluginBase {
         $this->subscribe('newSurveySettings');
     }
 
-    public function afterFindSurvey(){
+    public function afterFindSurvey() {
         $this->loadSurvey();
-        if(empty($this->survey)) {
+        if (empty($this->survey)) {
             return;
         }
         $surveyId = $this->survey->primaryKey;
 
-        $templatesEnabled = boolval($this->get("enabled",'Survey', $surveyId));
-        if(!$templatesEnabled) {
+        $templatesEnabled = boolval($this->get("enabled", 'Survey', $surveyId));
+        if (!$templatesEnabled) {
             return;
         }
 
-        $paramName = $this->get("paramName",'Survey', $surveyId);
-        if(empty($paramName)) {
+        $paramName = $this->get("paramName", 'Survey', $surveyId);
+        if (empty($paramName)) {
             return;
         }
 
         $templateKey = $this->api->getRequest()->getQuery($paramName);
-        $possibleTemplates = json_decode($this->get("templates",'Survey', $surveyId));
+        $possibleTemplates = json_decode($this->get("templates", 'Survey', $surveyId));
         $possibleTemplateKeys = array_keys((array) $possibleTemplates);
 
         if (!empty($templateKey) and in_array($templateKey, $possibleTemplateKeys)) {
@@ -57,14 +57,14 @@ class UrlTemplateChanger extends PluginBase {
         $templateName = $possibleTemplates->{$templateKey}->template;
         $allTemplates = array_keys($this->api->getTemplateList());
 
-        if(in_array($templateName,$allTemplates)){
-            $this->event->set('template',$templateName);
+        if (in_array($templateName, $allTemplates)) {
+            $this->event->set('template', $templateName);
         }
     }
 
     private function sessionKey()
     {
-        return self::SESSION_KEY."::" . $this->survey->primaryKey;
+        return self::SESSION_KEY."::".$this->survey->primaryKey;
     }
 
     private function loadSurvey()
@@ -83,7 +83,7 @@ class UrlTemplateChanger extends PluginBase {
             ->bindParam(':sid', $surveyId, PDO::PARAM_STR);
         $surveyArray = $query->queryRow();
 
-        if(empty($surveyArray)) {
+        if (empty($surveyArray)) {
             return;
         }
         $this->survey = (new Survey());
@@ -154,7 +154,7 @@ class UrlTemplateChanger extends PluginBase {
         {
             $this->set($name, $value, 'Survey', $event->get('survey'));
         }
-        $this->set('myTemplates', $event->get('settings')['templates'],'Survey',$event->get('survey'));
+        $this->set('myTemplates', $event->get('settings')['templates'], 'Survey', $event->get('survey'));
     }
 
 }
